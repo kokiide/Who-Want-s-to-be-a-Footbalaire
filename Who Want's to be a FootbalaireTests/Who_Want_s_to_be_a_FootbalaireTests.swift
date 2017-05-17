6abed5d8753f92fa769e7aa50d626690dd338f20
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-05-18 01:32:32 +0900

    Initial Commit

---
+++ Who Want's to be a FootbalaireTests/Who_Want_s_to_be_a_FootbalaireTests.swift
@@ -0,0 +1,36 @@
+//
+//  Who_Want_s_to_be_a_FootbalaireTests.swift
+//  Who Want's to be a FootbalaireTests
+//
+//  Created by Koki Ide on 2017/05/18.
+//  Copyright © 2017 Koki Ide. All rights reserved.
+//
+
+import XCTest
+@testable import Who_Want_s_to_be_a_Footbalaire
+
+class Who_Want_s_to_be_a_FootbalaireTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
